a8e5e4770917cf738952a3b229425fd0f828371d
jacoby <jacobyb0508@gmail.com>
2023-12-17 20:31:52 -0500

    Update build and source directories

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
 		extensions: [".ts", ".js"],
 	},
 	output: {
-		path: path.resolve(__dirname, "dist"),
+		path: path.resolve(__dirname, "build"),
 		filename: "[name].js",
 	},
 };
